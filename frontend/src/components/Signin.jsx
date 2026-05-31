05bac3faec5a67a702a844055f3f4d8c5cbc18de
WavyZany <130682641+WavyZany@users.noreply.github.com>
2026-05-31 01:16:46 -0400

    feat: basic email login functionality

---
+++ frontend/src/components/Signin.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { signInUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { session, error } = await signInUser(email, password);
+
+    if (error) {
+      setError(error);
+
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else {
+      navigate("/dashboard");
+    }
+
+    if (session) {
+      closeModal();
+      setError("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSignIn}>
+        <h2>Sign in</h2>
+
+        <p>
+          Don't have an account yet? <Link to="/signup">Sign up</Link>
+        </p>
+
+        <div>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <div>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit">Sign In</button>
+
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { signUpNewUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await signUpNewUser(email, password);
+
+      if (result.success) {
+        navigate("/dashboard");
+      } else {
+        setError(result.error.message);
+      }
+    } catch (err) {
+      setError("An unexpected error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSignUp}>
+        <h2>Sign up today!</h2>
+
+        <p>
+          Already have an account? <Link to="/">Sign in</Link>
+        </p>
+
+        <div>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <div>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" disabled={loading}>
+          Sign Up
+        </button>
+
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
